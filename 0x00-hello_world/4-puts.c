6c4fb7506edbceb095864b1aa4f66c86c610dfeb
jorgechauxjr <chauxjorge@hotmail.com>
2019-09-18 17:51:21 +0000

    code correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 #include <stdio.h>
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
  return (0);
 }
