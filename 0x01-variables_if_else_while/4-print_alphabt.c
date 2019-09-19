be8ef52f0c3bbd2dfd2dab8ebdc7432fcf274ce6
jorgechauxjr <chauxjorge@hotmail.com>
2019-09-19 23:59:46 +0000

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print the alphabet using putchar.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter = 'a';
+	char z = 'z';
+
+	while (letter <= z)
+	{
+		if (letter == 'q')
+		{
+			letter++;
+		}
+		else
+		{
+			if (letter == 'e')
+			{
+				letter++;
+}
+}
+		putchar (letter);
+		letter++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
