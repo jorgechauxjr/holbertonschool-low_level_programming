ebb8835615c03243a1350d58ca6966299cf82d92
jorgechauxjr <chauxjorge@hotmail.com>
2019-09-24 21:39:38 +0000

    commit

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,8 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*                                                                             * Structs, enums and unions definitions                                       * Typedefs                                                                    * Function prototypes                                                         */
+
+int largest_number(int a, int b, int c);
+void positive_or_negative(int i);
+#endif /*_HOLBERTON_H_ */
