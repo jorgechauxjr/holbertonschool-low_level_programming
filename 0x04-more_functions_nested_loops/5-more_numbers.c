63c98a8cefdd0b80c0bebd3009cb421ebd3441ad
jorgechauxjr <chauxjorge@hotmail.com>
2019-09-26 16:00:53 +0000

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * more_numbers -main entry.
+ * void: nothing
+ *Description: Prints 10 times the numbers from 0 to 14
+ * Return: void
+ **/
+
+
+void more_numbers(void)
+{
+	int cont, n;
+
+	for (cont = 0; cont < 10; cont++)
+	{
+		for (n = 0; n < 14; n++)
+		{
+			if (n > 9)
+				_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+	}
+
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -13,5 +13,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 
 #endif /*_HOLBERTON_H_ */
