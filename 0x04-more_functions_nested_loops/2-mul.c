c62626d585494fed20be11e857800f24500d0dc8
jorgechauxjr <chauxjorge@hotmail.com>
2019-09-26 14:53:26 +0000

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul -main entry.
+ * @a: integer to be checked
+ * @b: integer to be checked
+ *Description: Multiply two integers
+ * Return: multiply two integers
+ **/
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
