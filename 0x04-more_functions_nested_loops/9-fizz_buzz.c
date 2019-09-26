be0ae29b621dcb5f33dfc8aebf8853210a6db8dc
jorgechauxjr <chauxjorge@hotmail.com>
2019-09-26 20:58:27 +0000

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,22 +17,23 @@ int main(void)
 	{
 		if ((n % 3 != 0) && (n % 5 != 0))
 		{
-			printf("%d ", n);
+			printf("%d", n);
 		}
 		else
 		{
 			if ((n % 3 == 0) && (n % 5 == 0))
 			{
-				printf("FizzBuzz ");
+				printf("FizzBuzz");
 			}
 			else
 			{
 				if (n % 3 == 0)
-					printf("Fizz ");
+					printf("Fizz");
 				if (n % 5 == 0)
-					printf("Buzz ");
+					printf("Buzz");
 			}
 		}
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
