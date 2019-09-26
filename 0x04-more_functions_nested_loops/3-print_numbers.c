9c67d105a558b4b9b8483cc086c52d0d788d4864
jorgechauxjr <chauxjorge@hotmail.com>
2019-09-26 15:08:36 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers -main entry.
+ * void: integer to be checked
+ *Description: Prints numbers from 0 to 9
+ * Return:
+ **/
+
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	_putchar(n);
+
+	_putchar('\n');
+}
