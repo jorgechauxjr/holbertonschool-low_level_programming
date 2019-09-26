65b54a9e51ce2ecab2f82707083bf369d1df03c9
jorgechauxjr <chauxjorge@hotmail.com>
2019-09-26 14:32:18 +0000

    holberton.h file update

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif /*_HOLBERTON_H_ */
