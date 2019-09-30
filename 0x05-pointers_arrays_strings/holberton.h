55ca1374ca5a18492387a463c09e79cf797e2bc8
jorgechauxjr <chauxjorge@hotmail.com>
2019-09-30 21:54:32 +0000

    commit

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+void reset_to_98(int *n);
+
+#endif /*_HOLBERTON_H_ */
