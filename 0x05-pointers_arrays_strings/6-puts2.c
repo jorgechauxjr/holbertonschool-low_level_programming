ee97f114eda9b162bd2a2e3b79b52dab150da9cc
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-01 20:30:48 +0000

    conditional if is even

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,20 @@
 #include "holberton.h"
 /**
- *puts2 -main entry.
+ *puts2 -main entry
  *void: nothing
  *@str: char pointer
+ *
  *Description:Function prints every other character of a string
- * Return: void
- **/
+ *Return: void
+ */
 void puts2(char *str)
 {
 	int c;
 
 	for (c = 0; str[c] != '\0'; c++)
 	{
-
 		if (c % 2 == 0)
 		_putchar(str[c]);
-
-		c++;
-
-
 	}
 	_putchar('\n');
 }
