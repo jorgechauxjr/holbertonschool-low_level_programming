24d6e279ecfd657a2525b5d89b32ab00a75171a1
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-01 20:12:25 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,15 @@ void puts2(char *str)
 {
 	int c;
 
-	for (c = 0; str[c] != '\0'; c = c + 2)
+	for (c = 0; str[c] != '\0'; c++)
 	{
+
+		if (c % 2 == 0)
 		_putchar(str[c]);
+
+		c++;
+
+
 	}
 	_putchar('\n');
 }
