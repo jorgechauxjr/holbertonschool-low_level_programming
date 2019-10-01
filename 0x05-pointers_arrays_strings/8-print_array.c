99ec321dbf846994d73af84aceabcea0bedf150b
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-01 22:54:18 +0000

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_array -main entry.
+ *void: nothing
+ *@a: int pointer
+ *@n: integer number of elements of the array to be printed
+ *Description: function prints elements of array of integers
+ * Return: void
+ **/
+void print_array(int *a, int n)
+{
+	int c;
+
+	for (c = 0; c <= n; c++)
+	{
+		if (c == n)
+			printf("%d", a[c]);
+		else
+			printf("%d, ", a[c]);
+	}
+	printf("\n");
+}
