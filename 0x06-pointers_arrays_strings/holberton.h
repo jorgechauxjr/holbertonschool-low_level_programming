fcb764e31136064970eb259de703b7cad9dfdefd
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-03 14:36:26 +0000

    holberton.h and main 0 added

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif /*_HOLBERTON_H_ */
