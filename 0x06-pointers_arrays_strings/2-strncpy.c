6f3e67784e6a60a81db10441a7dfdc145525b509
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-03 17:05:14 +0000

    copy string function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ **_strncpy -main entry.
+ *void: nothing
+ *@dest: char pointer
+ *@src: char pointer
+ *@n: integer variable
+ *Description: function that copies a string.
+ * Return:  pointer to the resulting string dest
+ **/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c;
+
+	for (c = 0; c < n && src[c] != '0'; c++)
+		dest[c] = src[c];
+
+	return (dest);
+}
