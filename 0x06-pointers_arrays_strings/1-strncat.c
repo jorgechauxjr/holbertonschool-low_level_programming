05ca2500aff00f68ada7bb88e66f4aded917ddbb
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-03 16:37:27 +0000

    task 2 added

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ **_strncat -main entry.
+ *void: nothing
+ *@dest: char pointer
+ *@src: char pointer
+ *@n: integer variable
+ *Description: function that concatenates two strings.
+ * Return:  pointer to the resulting string dest
+ **/
+char *_strncat(char *dest, char *src, int n)
+{
+	int length = 0, c;
+
+	while (dest[length] != '\0')
+		length++;
+
+	for (c = 0; c < n; c++)
+		dest[length] = src[c];
+
+	return (dest);
+}
