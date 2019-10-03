72f53c7b1916dc8385a37cabb097badd2b7fc6f1
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-03 21:26:40 +0000

    rev array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *reverse_array -main entry.
+ *void: nothing
+ *@a: int  pointer
+ *@n: int variable
+ *Description: function that reverses the content of an array of integers
+ * Return:void
+ **/
+void reverse_array(int *a, int n)
+{
+/* n-- is n = n-1 because the last position is garbage*/
+	n--;
+
+	for (i = 0; i <= n; n--, i++)
+	{
+		ch = a[i];
+		a[i] = a[n];
+		a[n] = ch;
+	}
+}
