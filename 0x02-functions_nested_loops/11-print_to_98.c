632e759b93d85be8421fee5b803f52acce0b574a
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-05 17:48:07 +0000

    print to 98 func added

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - This functions adds two numbers
+ * @n:an integer number
+ * Description:function that prints all natural numbers from n to 98
+ * Return: void
+ **/
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	if (n > 98)
+	{
+		for ( ; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	if (n == 98)
+	{
+		printf("%d \n", n);
+	}
+}
