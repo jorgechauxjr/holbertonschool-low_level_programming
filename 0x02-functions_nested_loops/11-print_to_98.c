8b1b9e7108b0014b6118e920324d6fdbd6c0ba03
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-05 17:56:18 +0000

    code correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,18 +12,23 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			printf(",");
+			printf(" ");
+
 		}
 	}
 	if (n > 98)
 	{
 		for ( ; n > 98; n--)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			printf(",");
+			printf(" ");
 		}
 	}
 	if (n == 98)
 	{
-		printf("%d \n", n);
+		printf("%d\n", n);
 	}
 }
