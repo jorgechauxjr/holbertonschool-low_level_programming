acec0fd0383e171d02354494216a6eaf40215241
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-07 15:16:24 +0000

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ **_memcpy -main entry.
+ *@dest:char pointer
+ *@src: char pointer
+ *@n: integer variable
+ *Description: function that copies memory area.
+ * Return:char pointer
+ **/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (src);
+}
