9d5ed74b69d8a673ef0f2d47105cef78f6f53f71
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-07 18:43:50 +0000

    return correction

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -18,7 +18,7 @@ char *_strchr(char *s, char c)
 			return (&s[i]);
 		}
 	}
-	return (&s[i]);
+	return (null);
 /*
  *     &s[i] is equal to (s + 1) is a pointer, it means adress of.
  *      *(s+1) is equal to s[1] value of
