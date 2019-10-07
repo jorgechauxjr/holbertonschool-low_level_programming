747001d7f0328e7e6a972a86fd0ee49084fceb58
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-07 23:21:24 +0000

    holberton.h updated

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -13,5 +13,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+void set_string(char **s, char *to);
 
 #endif /*_HOLBERTON_H_ */
