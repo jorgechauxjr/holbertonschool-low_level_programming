12889cfb6d511ce84b3b9bfe57278ee0f45559a8
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-07 19:14:21 +0000

    commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,11 +14,11 @@ char *_strchr(char *s, char c)
 	for (i = 0; s != '\0'; i++)
 	{
 		if (s[i] == c)
-			return (s[i]);
+			return (s + i);
        }
 	if (s[i] == c)
-	return (s[i]);
-	return ('\0');
+	return (s + i);
+	return (0);
 /*
  *     &s[i] is equal to (s + 1) is a pointer, it means adress of.
  *      *(s+1) is equal to s[1] value of
