8553d64ce4f5d09206c92de0d7d1e283cdd8c36b
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-09 12:50:00 +0000

    holberton.h added

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif /*_HOLBERTON_H_ */
