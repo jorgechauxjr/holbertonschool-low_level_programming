98b29366777091744147f9a5b0d721e7860c023a
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-12 20:23:10 +0000

    func ischar added to 4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,37 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
+ *isChar -main entry.
+ *@p:pointer
+ *Description:function to see if its a char
+ * Return:0 if its a char or 1 otherwise
+ **/
+int isChar(char *p)
+{
+	int n;
+
+ 	while (*p != '\0')
+	{
+		n = *p;
+/*si es numero, mire la sig posicion*/
+/*si no es numero ischar es true devuelve 0 */
+		if (n > 47 && n < 58)
+		{
+			p++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+/*si es numero, ischar no se cumple, devuelve 1*/
+	return (1);
+}
+
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
  *main -main entry.
  *@argc:number of command line arguments
  *@argv: array containing the program command line arguments
@@ -10,7 +41,7 @@
  **/
 int main(int argc, char *argv[])
 {
-	int c;
+	int c, c2;
 	int s = 0;
 
 	(void)argc;
@@ -21,10 +52,18 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-	for (c = 1; c < argc; c++)
-		s += atoi(argv[c]);
+		for (c = 1; c < argc; c++)
+		{
+			if (isChar(argv[c]) == 0)
+			{
+				printf("Error\n");
+				return (0);
+			}
+		}
+		for (c2 = 1; c2 < argc; c2++)
+			s += atoi(argv[c2]);
 
-	printf("%d\n", s);
+			printf("%d\n", s);
 	}
 	return (0);
 }
