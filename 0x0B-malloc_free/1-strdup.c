b7dfb9671795180770b8af2af24574a793c23d5f
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-15 18:33:05 +0000

    1 strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Main entry
+ * @str: pointer
+ * Return: char pointer
+ */
+char *_strdup(char *str)
+{
+	char *cpstr;
+	int x;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		cpstr = malloc(sizeof(str));
+		if (cpstr != NULL)
+		{
+			for (x = 0; str[x] != '\0'; x++)
+			{
+				cpstr[x] = str[x];
+			}
+			return (cpstr);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+}
