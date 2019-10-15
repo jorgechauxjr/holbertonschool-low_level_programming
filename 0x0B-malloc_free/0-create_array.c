7e19ecce15599c37862e23fee22afe01cc2f9fd6
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-15 15:34:46 +0000

    commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Main entry
+ * @c: The character to print
+ *@size: unsigned integer
+ * Return: char.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int x;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		p = malloc(size * sizeof(c));
+
+		for (x = 0; x < size; x++)
+		{
+			p[x] = c;
+/*  *(p+x)=c     p[x]=c  */
+		}
+		return (p);
+	}
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif /*_HOLBERTON_H_ */
