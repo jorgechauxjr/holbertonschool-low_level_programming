018dfe6f58d67d22bc2358c1002e23c5af5270b5
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-17 19:13:58 +0000

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,28 +1,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 /**
- * _copystring - Main entry
- *@orig: pointer
- *@size: unsigned integer
- *Description: function that copies a string
- * Return:char pointer
- */
-
-
-char *_copystring(char *orig, unsigned int size)
-{
-	char *cp;
-	int c;
-
-	cp = malloc(sizeof(char) * size);
-
-	for (c = 0; orig[c] != '\0'; c++)
-		cp[c] = orig[c];
-
-	return (cp);
-}
-
-/**
  * string_nconcat - Main entry
  *@s1: pointer
  *@s2: pointer
@@ -32,37 +10,43 @@ char *_copystring(char *orig, unsigned int size)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int c1, c2, c3;
+	unsigned int c1, c2, c3, c4;
 	char *pconcat;
+
+/*c1 y c2 are lenght of s1 and s2*/
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	for (c1 = 0; s1[c1] != '\0'; c1++)
 		;
 
 	for (c2 = 0; s2[c2] != '\0'; c2++)
 		;
+
 	if (n >= c2)
+		n = c2;
+
+	pconcat = malloc(sizeof(char) * (c1 + n) + 1);
+
+	if (pconcat != NULL)
 	{
-		pconcat = malloc(sizeof(char) * c1 + sizeof(char) * c2);
+/* c3 counter acumulated, c4 para recorrer s1 desde cero*/
+		for (c3 = 0; s1[c3] != '\0'; c3++)
+			pconcat[c3] = s1[c3];
 
-		if (pconcat == NULL)
-			return (NULL);
+		for (c4 = 0; s2[c4] != '\0'; c4++, c3++)
+			pconcat[c3] = s2[c4];
 
-			pconcat = _copystring(s1, c1);
-
-		for (c3 = 0; s2[c3] != '\0'; c3++)
-			pconcat[c1] = s2[c3];
+		pconcat[c3] = '\0';
 
 		return (pconcat);
 	}
-	pconcat = malloc(sizeof(char) * c1 + sizeof(char) * c2);
-
-	if (pconcat == NULL)
+	else
+	{
 		return (NULL);
-
-		pconcat = _copystring(s1, c1);
-
-		for (c3 = 0; c3 < n; c3++)
-			pconcat[c1] = s2[c3];
-
-		return (pconcat);
+	}
 }
