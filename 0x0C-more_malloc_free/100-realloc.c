da4b4cbfd6451871b0f9f4fdf8807a22fc27c3fd
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-17 23:15:17 +0000

    100 realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * _realloc - Main entry
+ *@ptr: pointer
+ *@old_size: int
+ *@new_size: unsigned int
+ *Description:function that creates an array of integers.
+ * Return:int pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newptr;
+	unsigned int c;
+
+	if ((new_size == 0) && (ptr != NULL))
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		newptr = malloc(new_size)
+
+			if (newptr == NULL)
+				free(ptr);
+				return (NULL);
+	}
+	newptr = malloc(new_size);
+
+	if(newptr == NULL)
+		return (NULL);
+
+	for (c = 0; c < old_size; c++)
+		newptr[c] = ptr[c];
+
+	free(ptr);
+
+	return (newptr);
+}
