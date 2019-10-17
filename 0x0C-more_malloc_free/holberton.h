7800fd41b8d773851c2bd65ba8b9c459925947df
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-17 14:36:58 +0000

    holberton.h updated

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,16 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif /*_HOLBERTON_H_ */
