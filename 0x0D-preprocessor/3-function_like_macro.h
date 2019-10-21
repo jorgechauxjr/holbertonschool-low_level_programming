8ce9b0877fe07d604493a09fd0fb09b31e7dd051
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-21 19:49:57 +0000

    3-function_like_macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+#define ABS(x) (x < 0 ? (x) * (-1) : x)
+/* condition ? value returned if true : value returned if false*/
+#endif /*_FUNCTION_LIKE_MACRO_ */
