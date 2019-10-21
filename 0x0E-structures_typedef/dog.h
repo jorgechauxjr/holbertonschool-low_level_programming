46a8437516c081d86f0060e0cd69991a00b81659
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-21 21:37:55 +0000

    description added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef dog
+#define dog
+/**
+ *struct dog -main entry.
+ *@name: pointer to a char
+ *@age: float
+ *@owner: pointer to a char
+ *Description: types struct dog
+ **/
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* dog */
