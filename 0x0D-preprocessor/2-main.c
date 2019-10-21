dcd21aa385228222f6018169616f3f73b9981711
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-21 19:02:32 +0000

    description added

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-/*Program that prints the name of the file
-* it was compiled from*/
+/**
+ *main -main entry
+ *Description: Program that prints the name of the file
+ * it was compiled from
+ *Return: void
+ **/
 
-int main (void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 
