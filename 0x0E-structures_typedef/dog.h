c8baeabb7e4bc9cd7edc5119fac3bd423a4fa005
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-21 23:50:42 +0000

    typedef added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,8 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
