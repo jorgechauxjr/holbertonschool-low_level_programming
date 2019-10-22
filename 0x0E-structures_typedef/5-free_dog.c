b7004174016d66c7be319d22b842ad6a36099bfb
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-22 00:32:54 +0000

    5-free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dog
+ * @d: dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
