ca720586a0ec48f8738a42a4aa42a5c122313caf
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-23 18:27:43 +0000

    condition null added

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t c;
 
+	if (array != null && action != null)
+	{
 	for (c = 0; c < size; c++)
 		action(array[c]);
+	}
 
 }
