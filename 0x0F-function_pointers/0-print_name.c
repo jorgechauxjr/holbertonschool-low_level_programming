5a6f6c93db801a9f496bf24d6a06a62bc67250a8
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-23 17:08:59 +0000

    null entry validated

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,12 @@
 /**
  *print_name -main entry.
  *@name:char pointer
- *@f: oiubter to a function
+ *@f: pointer to a function
  *Description:Function that prints a string in reverse.
  * Return:void
  **/
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if(name != NULL && f != NULL)
+	 	(*f)(name);
 }
