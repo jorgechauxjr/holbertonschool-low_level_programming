c8c75f022d1ce12aab01300b3969208076e40d8c
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-23 22:35:57 +0000

    betty

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -39,7 +39,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	if( b == 0)
+	if (b == 0)
 	{
 		printf("Error\n");
 			exit(100);
@@ -54,7 +54,7 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	if( b == 0)
+	if (b == 0)
 	{
 		printf("Error\n");
 			exit(100);
