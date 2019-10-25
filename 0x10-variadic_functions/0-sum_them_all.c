b77743e5c29a68f2094418f3d6a7d8ac05ef2b7b
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-25 15:54:41 +0000

    0 sum them all and .h added

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all -main entry.
+ * @n: Constant unsigned integer
+ * Return: int sum of all parameters
+ **/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list my_list;
+	unsigned int c;
+	int sum;
+
+	va_start(my_list, n); /*Initialize the argument list*/
+
+	sum = 0;
+
+	for (c = 0; c < n; c++)
+		sum += va_arg(my_list, int); /*Get the next argument value*/
+
+	va_end(my_list); /*Clean up*/
+
+	return (sum);
+
+	}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif /*_VARIADIC_FUNCTIONS_H_*/
