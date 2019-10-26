5ea4446bc1d5a186a5e7a34e36840e320be656b1
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-26 18:24:19 +0000

    va end added

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -26,10 +26,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 				printf("%s", separator);
 		}
 
-		printf("\n");
-
 		va_end(my_list); /*Clean up*/
-
+		printf("\n");
 	}
 
 }
