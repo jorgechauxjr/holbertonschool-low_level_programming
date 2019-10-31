7d56ea974bd3e9682061e24ad9f92e938a940868
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-31 18:10:51 +0000

    add readme, 0 printlist and lists.h

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list -print list.
+ *Description: Function that prints content of a single linked list
+ * @h: pointer to a structure typedef list_t
+ * Return: the number of nodes
+ **/
+size_t print_list(const list_t *h)
+{
+	const list_t *temp = h;
+	size_t numNodes;
+
+	for (numNodes = 0; temp != NULL; numNodes++)
+	{
+		if (temp->str == NULL)
+		{
+		printf("[0] (nil)");
+		break;
+		}
+		else
+		{
+			printf("[%d] %s\n", temp->len, temp->str);
+			temp = temp->next;
+		}
+	}
+
+	return (numNodes);
+
+}
