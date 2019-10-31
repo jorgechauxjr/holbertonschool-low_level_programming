4208081e88238cdb479af922cfde3714ba621fd5
jorgechauxjr <chauxjorge@hotmail.com>
2019-10-31 18:30:08 +0000

    comments added

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,22 +9,23 @@
  **/
 size_t print_list(const list_t *h)
 {
+/*temp is a list_t type. It is a structure*/
 	const list_t *temp = h;
 	size_t numNodes;
+/*
+ * if temp is null, it means it is in the last node. For that reason the
+ * condition is: while temp diferent from NULL
+ */
 
 	for (numNodes = 0; temp != NULL; numNodes++)
 	{
 		if (temp->str == NULL)
-		{
-		printf("[0] (nil)\n");
-		}
+			printf("[0] (nil)\n");
+
 		else
-		{
 			printf("[%d] %s\n", temp->len, temp->str);
 
-		}
 		temp = temp->next; /*temp will point to the next node*/
 	}
-
 	return (numNodes);
 }
