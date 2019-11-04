14eba08e8421e3a4c2c2072b2052e284aa8bf35d
jorgechauxjr <chauxjorge@hotmail.com>
2019-11-04 20:00:37 +0000

    9 insert nodeint

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index -add node to list.
+ * Description: Function that adds a new node in an index position of list
+ * @head: pointer to a pointer
+ * @idx: int that represents the index, where the node is going to be inserted
+ * @n: integer value of the structure
+ * Return: the address of the new element or NULL if fail (the new node)
+ **/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newNode;
+	listint_t *temp = *head;
+
+	newNode = malloc(sizeof(listint_t)); /*Asign a memory space for newNode*/
+
+	if (newNode == NULL)
+	{ /* validate that malloc is good, if not return null and free memory */
+		free(newNode);
+		return (NULL);
+	}
+/*assign value of n to member n in the structure of new node*/
+	newNode->n = n;
+
+	if (idx == 0)
+	{
+/*the member newNode->next I assigned the addres of head*/
+		newNode->next = *head;
+		*head = newNode;
+	}
+	while (idx > 0)
+	{
+		if (temp == NULL)
+		{
+			free(newNode);
+			return (NULL);
+		}
+		temp = temp->next;
+		idx--;
+	}
+	newNode->next = temp->next;
+	temp->next = newNode;
+
+	return (newNode);
+}
