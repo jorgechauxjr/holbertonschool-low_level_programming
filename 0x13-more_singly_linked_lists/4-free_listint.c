8d9f59fd830994b0651eb8325d03ad3174add4d8
jorgechauxjr <chauxjorge@hotmail.com>
2019-11-04 18:38:47 +0000

    new option of code added in comments

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -12,8 +12,21 @@ void free_listint(listint_t *head)
 	if (head != NULL) /*validate that the pointer is not null*/
 	{
 		if (head->next != NULL)
-			free_listint(head->next);
+			free_listint(head->next); /* call recursive function */
 
 		free(head);
 	}
+/*
+ * other option:
+ *
+ *	listint_t *temp;
+ *
+ *	while (head != NULL)
+ *	{
+ *		temp = head;
+ *		head = head->next;
+ *		free(temp);
+ *	}
+ *      free(head)
+ **/
 }
