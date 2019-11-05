5fce699ee5d89f3f96a63c768f7ac5a7ec6c1630
jorgechauxjr <chauxjorge@hotmail.com>
2019-11-05 15:57:29 +0000

    loop while I added idx - 1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = newNode;
 		return (newNode);
 	}
-	while (idx > 0)
+	while ((idx - 1) > 0)
 	{
 		if (temp == NULL)
 		{
