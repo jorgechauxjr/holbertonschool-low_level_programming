79aaa0d95c89e14da029b986605ece8f3d413600
jorgechauxjr <chauxjorge@hotmail.com>
2019-11-08 00:13:28 +0000

    condition if idx is greater than the bits

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,19 +12,18 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask = 1;
 
+	if (index > 64) /*validate if idx is greater than the bits*/
+		return (-1); /*unsigned int 8 bytes x 8 = 64 bits*/
+
 	if (n == 0)
-	{
 		return (0);
-	}
+
 	mask = mask << index;
 
 	if (mask & n)
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
+
 	return (-1);
 }
