9ab5d7d65f7b3aa34aa85a6e8bb6568f23db590b
jorgechauxjr <chauxjorge@hotmail.com>
2019-11-08 13:38:14 +0000

    code optimization 2 get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,7 +7,6 @@
  *Description: function that returns the value of a bit at a given index.
  * Return: int, the bit you want to get
  **/
-
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask = 1;
