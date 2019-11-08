af2bc5a373faf74e7f69cfe832a79f95aa02765a
jorgechauxjr <chauxjorge@hotmail.com>
2019-11-08 01:11:18 +0000

    5 flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *flip_bits -.
+ *@n:bit number in poiunter char
+ *@m:is the index, starting from 0 of the bit you want to get
+ *Description: function that returns the number of bits you would need
+ * Return: 1 if succed -1 otherwise
+ **/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int c = 0;
+
+	n = n ^ m;
+
+	while (n > 0)
+	{
+		if (n & 1)
+		{
+			c++;
+		}
+		n = n >> 1; /*disminuir n*/
+	}
+
+	return (c);
+}
