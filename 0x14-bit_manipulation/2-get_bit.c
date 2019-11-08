c80ad1f8e72925c778858ceb0503e45715daf4a6
jorgechauxjr <chauxjorge@hotmail.com>
2019-11-08 00:09:24 +0000

    2 get bit added

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *get_bit -.
+ *@n: int number
+ *@index:is the index, starting from 0 of the bit you want to get
+ *Description: function that returns the value of a bit at a given index.
+ * Return: int, the bit you want to get
+ **/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	mask = mask << index;
+
+	if (mask & n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (-1);
+}
