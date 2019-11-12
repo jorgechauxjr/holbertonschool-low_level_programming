bb1ad7df11dd590b8617533cd623765182b4f8f7
jorgechauxjr <chauxjorge@hotmail.com>
2019-11-12 20:30:32 +0000

    0x15-file_io holberton.h include tags added

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,13 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
