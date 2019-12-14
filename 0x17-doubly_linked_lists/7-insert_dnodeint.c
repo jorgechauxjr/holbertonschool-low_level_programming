fdd5ddafa51f90de8e2bdaebb0f55b426eddc3ea
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-14 21:05:32 +0000

    0x17-doubly_linked_lists 7-insert_dnodeint.c unused len variable deleted

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,6 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *newNode, *temp = *h;
-	size_t len;
 newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 		return (NULL);
