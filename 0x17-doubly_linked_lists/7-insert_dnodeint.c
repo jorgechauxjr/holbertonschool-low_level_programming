8d5d46f9be4c061d72ad2267c50e4e0e46669dad
jorgechauxjr <chauxjorge@hotmail.com>
2019-12-15 00:48:21 +0000

    0x17-doubly_linked_lists 7-insert_dnodeint.c implementation of my own functions

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+ * dlistint_len - len.
+ *Description: Function that return number of nodes of a dsingle linked list
+ * @h: Head of the list. Pointer to a structure dlistint_sHead of the list.
+ * Return: the number of nodes
+ **/
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	size_t numNodes;
+
+	for (numNodes = 0; temp != NULL; numNodes++)
+	{
+		temp = temp->next;
+	}
+	return (numNodes);
+}
+/**
  * insert_dnodeint_at_index -add node to list.
  * Description: Function that adds a new node in an index position of list
  * @h: Head of the list. Pointer to a structure dlistint_s
@@ -12,11 +29,11 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *newNode, *temp = *h;
+	size_t listLen;
 newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = n;
-
 	if (*h == NULL)
 	{
 		if (idx > 0)
@@ -31,19 +48,16 @@ newNode = malloc(sizeof(dlistint_t));
 		}
 	if (idx == 0)
 	{
-		newNode->next = temp;
-		newNode->prev = NULL;
-		temp->prev = newNode;
-		*h = newNode;
-		return (newNode);
+		return (add_dnodeint(h, n));
 	}
+	listLen = dlistint_len(temp);
+	if (idx > listLen)
+		return (NULL);
 	while ((idx - 1) > 0)
 	{
 		if (temp == NULL)
-		{
 			free(newNode);
-			return (NULL);
-		}
+		return (NULL);
 		temp = temp->next;
 		idx--;
 	}
