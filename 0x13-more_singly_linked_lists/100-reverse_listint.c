4804b47cc4314ebdca9e4cf5472ee02c86e20c1a
jorgechauxjr <chauxjorge@hotmail.com>
2020-02-12 02:36:44 +0000

    Add 100-reverse listint Update lists.h

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * reverse_listint - Reverse a list
+ * @head: the head of list
+ * Description: this function reverse a singly linked list
+ * Return: head of the list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmp, *actual;
+
+	if (*head == NULL)
+		return (NULL);
+
+	actual = *head;
+
+	while (actual->next)
+	{
+		tmp = actual->next;
+		actual->next = tmp->next;
+		tmp->next = *head;
+		*head = tmp;
+	}
+	return (*head);
+}
