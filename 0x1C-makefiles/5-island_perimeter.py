b9ac3b1c364305085223020ea99db7e861a15413
jorgechauxjr <chauxjorge@hotmail.com>
2020-02-21 21:18:38 +0000

    Add 5-island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """ Returns the perimeter of the island described in grid"""
+
+    hg = len(grid)
+    wd= len(grid[0])
+    island = 0
+    edges = 0
+
+    for i in range(hg):
+        for j in range(wd):
+            if grid[i][j] == 1:
+                island += 1
+                if (j > 0 and grid[j][j - 1] == 1):
+                    edges = edges + 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges = edges + 1
+    print(island)
+    print(edges)
+    return (island * 4) - (edges * 2)
